093fd7d5645c940a1845557682f6ec7116dd069e
pb <pbgarnell25@gmail.com>
2025-03-04 19:37:19 -0600

    Add files via upload

---
+++ Player2D.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const SPEED = 400.0
+const JUMP_VEL = -400.0
+
+@onready var sprite = $Sprite2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VEL
+
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED / 2)
+
+	move_and_slide()
